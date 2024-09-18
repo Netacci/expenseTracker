fd47b3909c94c9c5d94d60290ba4adde92adf9d4
Chineta Adinnu <chinetaadinnu@gmail.com>
2024-09-18 07:42:15 +0100

    uodte google auth load

--- src/pages/auth/GooglePage.jsx
+++ src/pages/auth/GooglePage.jsx
@@ -1,13 +1,23 @@
+import { Loader2 } from 'lucide-react';
 import { useEffect } from 'react';
 
 const GooglePage = () => {
   useEffect(() => {
+    const prefetchGoogleAuth = new Image();
+    prefetchGoogleAuth.src = `${import.meta.env.VITE_APP_BASE_URL}auth/google`;
     const googleRedirect = async () => {
       window.location.href = `${import.meta.env.VITE_APP_BASE_URL}auth/google`;
     };
     googleRedirect();
   }, []);
-  return <div>Redirecting...</div>;
+  return (
+    <div>
+      <h2>Redirecting to Google Authentication...</h2>
+      <div className='w-full h-full flex justify-center items-center'>
+        <Loader2 className='w-10 h-10 animate-spin' />
+      </div>
+    </div>
+  );
 };
 
 export default GooglePage;
