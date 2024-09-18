3f97b5bfefc5fdb95541e535d6f6e31da945c539
Chineta Adinnu <chinetaadinnu@gmail.com>
2024-09-18 14:53:09 +0100

    updates

--- src/pages/auth/GooglePage.jsx
+++ src/pages/auth/GooglePage.jsx
@@ -3,8 +3,6 @@ import { useEffect } from 'react';
 
 const GooglePage = () => {
   useEffect(() => {
-    const prefetchGoogleAuth = new Image();
-    prefetchGoogleAuth.src = `${import.meta.env.VITE_APP_BASE_URL}auth/google`;
     const googleRedirect = async () => {
       window.location.href = `${import.meta.env.VITE_APP_BASE_URL}auth/google`;
     };
